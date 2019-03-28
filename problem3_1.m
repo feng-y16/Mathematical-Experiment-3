501cc45879e8dd9cac8befdf0653c97164d15d7b
Feng Yao <46351482+feng-y16@users.noreply.github.com>
2019-03-28 22:41:59 +0800

    Add files via upload

--- problem3_1.m
+++ problem3_1.m
@@ -1,28 +1,37 @@
 n=5;
+t1=zeros(3,1);
 [V,b_v] = Vandermonde_and_b(n);
 [H,b_h] = Hilbert_and_b(n);
+tic
 error_v=norm(V\b_v-ones(n,1),2);
 error_h=norm(H\b_h-ones(n,1),2);
-fprintf("error_v=%f\n",error_v);
-fprintf("error_h=%f\n",error_h);
+t1(1)=toc;
+tic
+error_v_Jacobi=norm(Jacobi(V,b_v,1e+100,error_v,0)-ones(n,1),2);
+error_h_Jacobi=norm(Jacobi(H,b_h,1e+100,error_h,0)-ones(n,1),2);
+t1(2)=toc;
+tic
+error_v_GS=norm(GS(V,b_v,1e+100,error_v*1e-10,0)-ones(n,1),2);
+error_h_GS=norm(GS(H,b_h,1e+100,error_h*1e-10,0)-ones(n,1),2);
+t1(3)=toc;
 
 epsilon=[1e-10,1e-8,1e-6];
 tol=1e-5;
 m=2000;
 x0=0;
 
-t=zeros(4,1);
+t2=zeros(4,1);
 tic
 data=collect_perturb_data(@purturb,epsilon,4);
-t(1)=toc;
+t2(1)=toc;
 tic
 data_Jacobi=collect_perturb_data(@purturb_Jacobi,epsilon,4,tol,m,x0);
-t(2)=toc;
+t2(2)=toc;
 tic
 data_GS=collect_perturb_data(@purturb_GS,epsilon,4,tol,m,x0);
-t(3)=toc;
+t2(3)=toc;
 tic
 data_PCG=collect_perturb_data(@purturb_PCG,epsilon,4,tol,m,x0);
-t(4)=toc;
+t2(4)=toc;
 
 norm_Jacobi_GS=compute_norm(4);
